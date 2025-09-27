37cb4740cd0927e1ae558b25485388332a89032a
aiko <aiko@aikos-MacBook-Pro.local>
2025-09-27 16:03:32 -0500

    1

--- studio/studio_5/readme.txt
+++ studio/studio_5/readme.txt
@@ -1,2 +1,16 @@
 1. Zichu Pan
 2. 
+    Code:
+        const char* str1 = "Hello, ";
+        const char* str2 = "World!";
+        
+        std::cout << "First string: \"" << str1 << "\"" << std::endl;
+        std::cout << "Second string: \"" << str2 << "\"" << std::endl;
+        
+        std::cout << "Length of first string: " << std::strlen(str1) << std::endl;
+        std::cout << "Length of second string: " << std::strlen(str2) << std::endl;
+    Output:
+        First string: "Hello, "
+        Second string: "World!"
+        Length of first string: 7
+        Length of second string: 6
--- studio/studio_5/studio5.cpp
+++ studio/studio_5/studio5.cpp
@@ -4,12 +4,14 @@
 int main() {
     const char* str1 = "Hello, ";
     const char* str2 = "World!";
+
+    std::size_t (*func_ptr)(const char*) = std::strlen;
     
     std::cout << "First string: \"" << str1 << "\"" << std::endl;
     std::cout << "Second string: \"" << str2 << "\"" << std::endl;
     
-    std::cout << "Length of first string: " << std::strlen(str1) << std::endl;
-    std::cout << "Length of second string: " << std::strlen(str2) << std::endl;
+    std::cout << "Length of first string: " << (*func_ptr)(str1) << std::endl;
+    std::cout << "Length of second string: " << (*func_ptr)(str2) << std::endl;
     
     return 0;
 }
