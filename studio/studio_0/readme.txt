1996edb606391864eace03a89049f5a43472566c
aiko <aiko@aikos-MacBook-Pro.local>
2025-09-27 12:28:25 -0500

    1

--- studio/studio_0/readme.txt
+++ studio/studio_0/readme.txt
@@ -1,7 +1,7 @@
 1. Zichu Pan
 2. hostname: iht32-1501.engr.wustl.edu
 3. 
-    which gcc: /usr/bin/gcc
-    g++ -v: gcc version 8.5.0 20210514 (Red Hat 8.5.0-22) (GCC)
-4.
-    
+    which gcc: /project/compute/compilers/gcc-8.3.0/bin/gcc
+    g++ -v: gcc version 8.3.0 (GCC)
+4. output: hello, world!
+5.
--- studio/studio_0/studio0.cpp
+++ studio/studio_0/studio0.cpp
@@ -6,9 +6,14 @@
 using namespace std; // optional, could say std::cout and std::endl below instead
 
 const int SUCCESS = 0; // by convention, where non-zero results are error codes
+const int NO_ARGUMENTS = 1;
 
-int main (int, char**) { // argc and argv not used, but their types are specified
-
+int main (int argc, char** argv) { // argc and argv not used, but their types are specified
+    if (argc < 2) {
+        cout << "usage: " << argv[0] << " <arg1> [<arg2> ...]" << endl;
+        return NO_ARGUMENTS;
+    }
+    
     cout << "hello, world!" << endl; // procedural: has an effect
 
     return SUCCESS; // functional: returns a success/failure code
