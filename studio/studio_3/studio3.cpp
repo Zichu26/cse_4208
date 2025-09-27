b51326a5efbac350d6cf0ea59f53a02a1b8335a5
aiko <aiko@aikos-MacBook-Pro.local>
2025-09-27 14:35:39 -0500

    1

--- studio/studio_3/studio3.cpp
+++ studio/studio_3/studio3.cpp
@@ -2,38 +2,13 @@
    of Kernighan and Ritchie's The C Programming Language, 2nd. Ed. */
 
 #include <iostream> // for cout ostream and endl manipulator
-#include "Weekday.h"
 
 using namespace std; // optional, could say std::cout and std::endl below instead
 
 const int SUCCESS = 0; // by convention, where non-zero results are error codes
 
 int main (int argc, char** argv) { // argc and argv not used, but their types are specified
-    Weekday today = Weekday::Wednesday;
-    std::cout << today << std::endl;
-    Weekday start = today;
-
-    do {
-        std::cout << today;
-        today++;
-    } while (today != start);
-    
-    std::cout << std::endl;
-
-    do {
-        std::cout << today;
-        today--;
-    } while (today != start);
-    
-    std::cout << std::endl;
-
-    Pressure currentPressure = Pressure::pop;
-    std::cout << currentPressure << std::endl;
-
-    for (Pressure p = Pressure::lo; p != Pressure::pop; ++p) {
-        std::cout << p;
-    }
-    std::cout << std::endl;
+    cout << ":3" << endl;
 
     return SUCCESS; // functional: returns a success/failure code
 }
