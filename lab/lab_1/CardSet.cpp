1736e470769461e918fbdb03ad7c1334882f23fb
aiko <aiko@aikos-MacBook-Pro.local>
2025-09-27 17:41:05 -0500

    1

--- lab/lab_1/CardSet.cpp
+++ lab/lab_1/CardSet.cpp
@@ -3,7 +3,7 @@
 #include "CardSet.h"
 
 template <typename R, typename S>
-void CardSet<R, S>::print(std::ostream& os, size_t cardInRow) {
+void CardSet<R, S>::print(std::ostream& os, size_t cardInRow) const {
     size_t i = 0; // card index
     
     for (typename std::vector< Card<R, S> >::iterator card = cards.begin(); card != cards.end(); ++card) {
@@ -12,13 +12,14 @@ void CardSet<R, S>::print(std::ostream& os, size_t cardInRow) {
             os << std::endl;
         }
         os << *card;    // de-reference since card is an iterator
-        '''
+        /*
             (i + 1) % cardInRow != 0 --> true if card is not at end of row
             std::next(card) != cards.end() --> true if card is not last card
         '''
         if ((i + 1) % cardInRow != 0 && std::next(card) != cards.end()) {
             os << " ";
         }
+        */
         ++i;
     }
     if (i > 0) {
