6f9c22e0c423e034545b13b0b82b4c557006ebbd
aiko <aiko@aikos-MacBook-Pro.local>
2025-09-27 15:22:29 -0500

    1

--- studio/studio_4/studio4.cpp
+++ studio/studio_4/studio4.cpp
@@ -16,7 +16,6 @@ int main (int argc, char** argv) { // argc and argv not used, but their types ar
     std::cout << "Testing precedence: " << ++p << std::endl;
     std::cout << "Final value: " << p << std::endl;
 
-    Pressure p = Pressure::lo;
     Pressure p1 = Pressure::med;
     Pressure p2 = Pressure::hi;
     
