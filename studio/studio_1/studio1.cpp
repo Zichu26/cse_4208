2d6eb74ab0e568c2e6a371b35a47534a0f406341
aiko <aiko@aikos-MacBook-Pro.local>
2025-09-27 14:33:00 -0500

    1

--- studio/studio_1/studio1.cpp
+++ studio/studio_1/studio1.cpp
@@ -10,14 +10,7 @@ const int SUCCESS = 0; // by convention, where non-zero results are error codes
 
 int main (int argc, char** argv) { // argc and argv not used, but their types are specified
 
-    MyStruct<> obj1(42);
-    MyStruct<> obj2(17);
-    
-    cout << obj1 << " " << obj2 << endl;
-    
-    std::swap(obj1, obj2);
-    
-    cout << obj1 << " " << obj2 << endl;
+
 
     return SUCCESS; // functional: returns a success/failure code
 }
