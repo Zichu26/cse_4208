dfa5d03b99369e647ca2c6b4e81b0c664c455944
aiko <aiko@aikos-MacBook-Pro.local>
2025-09-27 12:55:26 -0500

    1

--- studio/studio_1/MyStruct.h
+++ studio/studio_1/MyStruct.h
@@ -5,6 +5,10 @@ struct MyStruct {
     int value;
     
     MyStruct(int v);
+
+    MyStruct(const MyStruct&) = delete;
+    MyStruct& operator=(const MyStruct&) = delete;
+    // ~MyStruct() = delete;
 };
 
 #endif // MYSTRUCT_H
--- studio/studio_1/readme.txt
+++ studio/studio_1/readme.txt
@@ -1,2 +1,11 @@
 1. Zichu Pan
 2. 
+    Code:
+        MyStruct obj1(1);
+        MyStruct obj2(2);
+        
+        cout << obj1.value << " " << obj2.value << endl;
+    Output:
+        1 2
+3. 
+    
