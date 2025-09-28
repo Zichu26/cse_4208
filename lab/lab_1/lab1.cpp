f9f9c2790fd1a28b01b798446bc22157872ffa7c
aiko <aiko@aikos-MacBook-Pro.local>
2025-09-27 19:29:44 -0500

    1

--- lab/lab_1/lab1.cpp
+++ lab/lab_1/lab1.cpp
@@ -5,34 +5,8 @@
 #include "HoldEmDeck.h"
 
 int main() {
-    PinochleDeck pinochleDeck;
-    HoldEmDeck holdEmDeck0;
-    HoldEmDeck holdEmDeck1;
-
-    std::cout << "Pinochle Deck:" << std::endl;
-    pinochleDeck.print(std::cout, 6);
-    std::cout << std::endl;
-
-    std::cout << "Texas Hold 'Em Deck:" << std::endl;
-    holdEmDeck0.print(std::cout, 13);
-    std::cout << std::endl;
-
-    pinochleDeck.shuffle();
-    std::cout << "Pinochle Deck:" << std::endl;
-    pinochleDeck.print(std::cout, 6);
-    std::cout << std::endl;
-
-    holdEmDeck0.shuffle();
-    std::cout << "Texas Hold 'Em Deck0:" << std::endl;
-    holdEmDeck0.print(std::cout, 13);
-    std::cout << std::endl;
-
-    holdEmDeck0 >> holdEmDeck1;
-    std::cout << "Texas Hold 'Em Deck0:" << std::endl;
-    holdEmDeck0.print(std::cout, 13);
-
-    std::cout << "Texas Hold 'Em Deck1:" << std::endl;
-    holdEmDeck1.print(std::cout, 13);
+    PinochleGame tmp(3, ["Peter", "Jack", "Dom"]);
+    tmp.play();
 
     return 0;
 }
