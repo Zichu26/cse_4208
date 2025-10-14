e3bd802e8eda210270cb0f75dd5029fdcf262615
aiko <zichupan26@gmail.com>
2025-10-13 20:14:28 -0500

    1

---
+++ studio/studio_7/Weekday.cpp
@@ -0,0 +1,121 @@
+#include "Weekday.h"
+
+std::ostream& operator<<(std::ostream& os, const Weekday& day) {
+    switch (day) {
+        case Weekday::Sunday:
+            os << "Sun";
+            break;
+        case Weekday::Monday:
+            os << "Mon";
+            break;
+        case Weekday::Tuesday:
+            os << "Tue";
+            break;
+        case Weekday::Wednesday:
+            os << "Wed";
+            break;
+        case Weekday::Thursday:
+            os << "Thu";
+            break;
+        case Weekday::Friday:
+            os << "Fri";
+            break;
+        case Weekday::Saturday:
+            os << "Sat";
+            break;
+    }
+    
+    os << " ";
+    return os;
+}
+
+Weekday operator++(Weekday& day, int) {
+    switch (day) {
+        case Weekday::Sunday:
+            day = Weekday::Monday;
+            break;
+        case Weekday::Monday:
+            day = Weekday::Tuesday;
+            break;
+        case Weekday::Tuesday:
+            day = Weekday::Wednesday;
+            break;
+        case Weekday::Wednesday:
+            day = Weekday::Thursday;
+            break;
+        case Weekday::Thursday:
+            day = Weekday::Friday;
+            break;
+        case Weekday::Friday:
+            day = Weekday::Saturday;
+            break;
+        case Weekday::Saturday:
+            day = Weekday::Sunday; 
+            break;
+    }
+    return day;
+}
+
+Weekday operator--(Weekday& day, int) {
+    switch (day) {
+        case Weekday::Sunday:
+            day = Weekday::Saturday; 
+            break;
+        case Weekday::Monday:
+            day = Weekday::Sunday;
+            break;
+        case Weekday::Tuesday:
+            day = Weekday::Monday;
+            break;
+        case Weekday::Wednesday:
+            day = Weekday::Tuesday;
+            break;
+        case Weekday::Thursday:
+            day = Weekday::Wednesday;
+            break;
+        case Weekday::Friday:
+            day = Weekday::Thursday;
+            break;
+        case Weekday::Saturday:
+            day = Weekday::Friday;
+            break;
+    }
+    return day;
+}
+
+std::ostream& operator<<(std::ostream& os, const Pressure& pressure) {
+    switch (pressure) {
+        case Pressure::lo:
+            os << "Low";
+            break;
+        case Pressure::med:
+            os << "Medium";
+            break;
+        case Pressure::hi:
+            os << "High";
+            break;
+        case Pressure::pop:
+            os << "Pop";
+            break;
+    }
+    
+    os << " ";
+    return os;
+}
+
+Pressure& operator++(Pressure& pressure) {
+    switch (pressure) {
+        case Pressure::lo:
+            pressure = Pressure::med;
+            break;
+        case Pressure::med:
+            pressure = Pressure::hi;
+            break;
+        case Pressure::hi:
+            pressure = Pressure::pop;
+            break;
+        case Pressure::pop:
+            break;
+    }
+    return pressure;
+}
---
+++ studio/studio_7/Weekday.h
@@ -0,0 +1,30 @@
+#ifndef WEEKDAY_H
+#define WEEKDAY_H
+
+#include <ostream>
+
+enum class Weekday {
+    Sunday,
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday
+};
+
+enum class Pressure {
+    lo,
+    med,
+    hi,
+    pop
+};
+
+std::ostream& operator<<(std::ostream& os, const Pressure& pressure);
+std::ostream& operator<<(std::ostream& os, const Weekday& day);
+// int is a dummy parameter used solely to distinguish it from the prefix increment operator.
+Weekday operator++(Weekday& day, int);  
+Weekday operator--(Weekday& day, int);
+Pressure& operator++(Pressure& pressure); 
+
+#endif // WEEKDAY_H
