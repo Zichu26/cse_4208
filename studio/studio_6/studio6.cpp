0c9338880cb0b001b7b7fa33c7fa2917da0c3fc1
aiko <zichupan26@gmail.com>
2025-10-13 19:28:56 -0500

    1

--- studio/studio_6/studio6.cpp
+++ studio/studio_6/studio6.cpp
@@ -7,10 +7,11 @@ int main() {
     std::cout << "y: " << s1.getY() << std::endl;
     s1.setX(33);
     s1.setY(44);
-    std::cout << "\nnon const after mutation:" << std::endl;
+    std::cout << "non const after mutation:" << std::endl;
     std::cout << "x: " << s1.getX() << std::endl;
     std::cout << "y: " << s1.getY() << std::endl;
     
+    std::cout << std::endl;
     const MyStruct s2;
     std::cout << "x: " << s2.getX() << std::endl;
     std::cout << "y: " << s2.getY() << std::endl;
