85591cd22cad76bfa651fd7ad85e3fd694bff619
aiko <zichupan26@gmail.com>
2025-10-13 19:35:42 -0500

    1

--- studio/studio_6/studio6.cpp
+++ studio/studio_6/studio6.cpp
@@ -22,7 +22,7 @@ int main() {
     cout << "m.x is " << m.x() << " and m.y is " << m.y() << endl;
     m.x(7).y(3); // chained use of mutator functions
     cout << "m.x is " << m.x() << " and m.y is " << m.y() << endl;
-    MyClass n(m); // copy construction
+    MyStuct n(m); // copy construction
     cout << "n.x = " << n.x() << " and n.y = " << n.y() << endl;
     
 }
